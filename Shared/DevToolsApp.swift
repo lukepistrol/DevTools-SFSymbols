c63a28164a7353bb87957bb4f41a18f2cd4b10af
Luke <l.pistrol@gmail.com>
2022-01-19 23:47:09 +0100

    Initial Commit

---
+++ Shared/DevToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevToolsApp.swift
+//  Shared
+//
+//  Created by Lukas Pistrol on 19.01.22.
+//
+
+import SwiftUI
+
+@main
+struct DevToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
